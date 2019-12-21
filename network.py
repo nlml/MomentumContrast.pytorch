5898bb7ed5866a6eae684f2272e4328d4ad7229d
liam schoneveld <liam.sch@gmail.com>
2019-12-22 00:57:18 +0100

    add semisup stuff

--- network.py
+++ network.py
@@ -3,18 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, sup_out=False):
         super(Net, self).__init__()
+        self.sup_out = sup_out
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.fc1 = nn.Linear(9216, 128)
+        if self.sup_out:
+            self.fc_sup = nn.Linear(128, 10)
 
-    def forward(self, x):
+    def forward(self, x, sup=False):
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
         x = F.max_pool2d(x, 2)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
+        if sup:
+            return self.fc_sup(x)
         x = F.normalize(x)
         return x
---
+++ test_logreg.py
@@ -0,0 +1,69 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import numpy as np
+import tqdm
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from network import Net
+import torch
+import torch.utils.data as tud
+from sklearn.linear_model import LogisticRegression
+
+
+def get_all_x(mnist):
+    loader = tud.DataLoader(mnist, batch_size=100, num_workers=8)
+    all_x = []
+    for x, y in tqdm.tqdm(loader):
+        with torch.no_grad():
+            all_x.append(model(x.to(device)).cpu())
+    return torch.cat(all_x)
+
+
+def show(mnist, targets, ret):
+    target_ids = range(len(set(targets)))
+    
+    colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'violet', 'orange', 'purple']
+    
+    plt.figure(figsize=(12, 10))
+    
+    ax = plt.subplot(aspect='equal')
+    for label in set(targets):
+        idx = np.where(np.array(targets) == label)[0]
+        plt.scatter(ret[idx, 0], ret[idx, 1], c=colors[label], label=label)
+    
+    for i in range(0, len(targets), 250):
+        img = (mnist[i][0] * 0.3081 + 0.1307).numpy()[0]
+        img = OffsetImage(img, cmap=plt.cm.gray_r, zoom=0.5) 
+        ax.add_artist(AnnotationBbox(img, ret[i]))
+    
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MoCo example: MNIST')
+    parser.add_argument('--model', '-m', default='result/model.pth',
+                        help='Model file')
+    args = parser.parse_args()
+    model_path = args.model
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))])
+
+    mnist = datasets.MNIST('./', train=True, download=True, transform=transform)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = Net().to(device)
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    mnist_test = datasets.MNIST('./', train=False, download=True, transform=transform)
+    all_x = get_all_x(mnist)
+    all_x_test = get_all_x(mnist_test)
+    for C in np.logspace(-1, 9, 6):
+        print()
+        print('C =', C)
+        lr = LogisticRegression(C=C, solver='lbfgs', multi_class='multinomial', max_iter=500)
+        lr.fit(all_x, mnist.targets)
+        print((lr.predict(all_x_test) == mnist_test.targets.numpy()).mean())
